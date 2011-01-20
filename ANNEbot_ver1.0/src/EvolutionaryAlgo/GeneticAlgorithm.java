f94bd5df9dac56ef9e70f7afb0e00802ae30c426
Sulantha <sulantha@sulantha-virtual-machine.(none)>
2011-01-20 14:04:39 +0530

    added genalgo file

---
+++ ANNEbot_ver1.0/src/EvolutionaryAlgo/GeneticAlgorithm.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package EvolutionaryAlgo;
+import Utility.*;
+
+
+/**
+ *
+ * @author sulantha
+ */
+public class GeneticAlgorithm {
+    Matrix[] genomePopulation;
+    int sizeOfPopulation;
+    int genomeLength;
+    int sumOfFitness;
+    int bestFitness;
+    int avaerageFitness;
+    int worstFitness;
+    int indexOfBestGenome;
+
+    double crossoverRate;
+    double mutationRate;
+
+    int Generation;
+
+    
+    
+
+}
--- ANNEbot_ver1.0/src/EvolutionaryAlgo/Genome.java
+++ ANNEbot_ver1.0/src/EvolutionaryAlgo/Genome.java
@@ -4,11 +4,24 @@
  */
 
 package EvolutionaryAlgo;
-
+import Utility.*;
 /**
  *
  * @author dilmi
  */
 public class Genome {
+    Matrix weightMatrix;
+    double fitnessValue;
+
+    Genome(){
+        fitnessValue = 0;
+    }
+
+    Genome(Matrix passweights, double passfitness){
+        weightMatrix = passweights;
+        fitnessValue = passfitness;
+    }
+
+    
 
 }
