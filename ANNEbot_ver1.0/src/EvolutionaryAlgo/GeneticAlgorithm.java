468efe89c6785fbdcc1063a430a80c157a16115e
Sulantha <sulantha@sulantha-virtual-machine.(none)>
2011-01-21 21:02:39 +0530

    changed EvoAlgo file added roulette wheel and mutation

--- ANNEbot_ver1.0/src/EvolutionaryAlgo/GeneticAlgorithm.java
+++ ANNEbot_ver1.0/src/EvolutionaryAlgo/GeneticAlgorithm.java
@@ -5,6 +5,7 @@
 
 package EvolutionaryAlgo;
 import Utility.*;
+import java.util.Random;
 
 
 /**
@@ -12,7 +13,7 @@ import Utility.*;
  * @author sulantha
  */
 public class GeneticAlgorithm {
-    Matrix[] genomePopulation;
+    Genome[] genomePopulation;
     int sizeOfPopulation;
     int genomeLength;
     int sumOfFitness;
@@ -21,12 +22,40 @@ public class GeneticAlgorithm {
     int worstFitness;
     int indexOfBestGenome;
 
+    double maxMutationFactor = 0.3;
     double crossoverRate;
     double mutationRate;
 
     int Generation;
 
-    
+
+    Genome getGenomeRoulette(){
+        Genome theChosenOne = null;
+        Random randomNum = new Random();
+        double value = randomNum.nextDouble()*sumOfFitness;
+        double cumalativeFitness = 0;
+        for(int i = 0; i < sizeOfPopulation; i++){
+            cumalativeFitness = cumalativeFitness + genomePopulation[i].fitnessValue;
+            if (cumalativeFitness > value){
+                theChosenOne = genomePopulation[i];
+                break;
+            }
+        }
+        return theChosenOne;
+    }
+
+    Genome mutate(Genome origGenome){
+        Genome mutatedGenome = null;
+        for (int i = 0; i < genomeLength; i++){
+            Random randomNum = new Random();
+            double value = randomNum.nextDouble();
+            if (value <= mutationRate){
+                origGenome.weightMatrix.set(0, i, origGenome.weightMatrix.get(0, i)+(randomNum.nextDouble()-randomNum.nextDouble())*maxMutationFactor);
+            }
+        }
+        mutatedGenome = origGenome;
+        return mutatedGenome;
+    }
     
 
 }
