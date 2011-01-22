27aaea0040a04bc3622f1b29644bb36638519e96
Sulantha <sulantha@sulantha-virtual-machine.(none)>
2011-01-22 09:33:48 +0530

    modified EvoAlgo

--- ANNEbot_ver1.0/src/EvolutionaryAlgo/GeneticAlgorithm.java
+++ ANNEbot_ver1.0/src/EvolutionaryAlgo/GeneticAlgorithm.java
@@ -5,6 +5,7 @@
 
 package EvolutionaryAlgo;
 import Utility.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -13,8 +14,8 @@ import java.util.Random;
  * @author sulantha
  */
 public class GeneticAlgorithm {
-    Genome[] genomePopulation;
-    int sizeOfPopulation;
+    ArrayList<Genome> genomePopulation = new ArrayList<Genome>();
+    int sizeOfPopulation = genomePopulation.size();
     int genomeLength;
     int sumOfFitness;
     int bestFitness;
@@ -35,9 +36,9 @@ public class GeneticAlgorithm {
         double value = randomNum.nextDouble()*sumOfFitness;
         double cumalativeFitness = 0;
         for(int i = 0; i < sizeOfPopulation; i++){
-            cumalativeFitness = cumalativeFitness + genomePopulation[i].fitnessValue;
+            cumalativeFitness = cumalativeFitness + genomePopulation.get(i).fitnessValue;
             if (cumalativeFitness > value){
-                theChosenOne = genomePopulation[i];
+                theChosenOne = genomePopulation.get(i);
                 break;
             }
         }
@@ -45,7 +46,6 @@ public class GeneticAlgorithm {
     }
 
     Genome mutate(Genome origGenome){
-        Genome mutatedGenome = null;
         for (int i = 0; i < genomeLength; i++){
             Random randomNum = new Random();
             double value = randomNum.nextDouble();
@@ -53,9 +53,36 @@ public class GeneticAlgorithm {
                 origGenome.weightMatrix.set(0, i, origGenome.weightMatrix.get(0, i)+(randomNum.nextDouble()-randomNum.nextDouble())*maxMutationFactor);
             }
         }
-        mutatedGenome = origGenome;
-        return mutatedGenome;
+        return origGenome;
     }
+
+    Genome[] crossover(Genome mom, Genome dad){
+        Genome[] babyArray = new Genome[2];
+
+        Random randomNum = new Random();
+        if ((randomNum.nextDouble()>crossoverRate)||(mom == dad)){
+            babyArray[0] = mom;
+            babyArray[1] = dad;
+
+            return babyArray;
+        }
+
+        int crossoverPoint = randomNum.nextInt(genomeLength - 1);
+
+        for (int i = 0; i < crossoverPoint; i++){
+            babyArray[0].weightMatrix.set(0, i, mom.weightMatrix.get(0, i));
+            babyArray[1].weightMatrix.set(0, i, dad.weightMatrix.get(0, i));
+        }
+
+        for (int i = crossoverPoint; i < genomeLength; i++){
+            babyArray[0].weightMatrix.set(0, i, dad.weightMatrix.get(0, i));
+            babyArray[1].weightMatrix.set(0, i, mom.weightMatrix.get(0, i));
+        }
+
+        return babyArray;
+    }
+
+
     
 
 }
