205fadd5e47b10378b0e0df68fcf58a641c6801a
Sulantha <sulantha@sulantha-virtual-machine.(none)>
2011-01-27 13:54:08 +0530

    modified EvoMan

--- ANNEbot_ver1.0/src/EvolutionaryAlgo/EvolutionManager.java
+++ ANNEbot_ver1.0/src/EvolutionaryAlgo/EvolutionManager.java
@@ -14,17 +14,35 @@ import java.util.ArrayList;
  */
 public class EvolutionManager {
     int populationSize = 10;
+    int genomeIndex = 0;
+    ArrayList<Genome> oldPopulation = new ArrayList<Genome>();
+    ArrayList<Genome> newPopulation = new ArrayList<Genome>();
 
 
-    Genome createGenome(ANN oldANN){
-        return new Genome(getWeights(oldANN),oldANN.getFitness());
+    Genome createGenome(Matrix weights, double fitness){
+        return new Genome(weights, fitness);
     }
 
     public ANN evolveANN(ANN oldANN, int genCount){
         if (genCount == 0){
             createInitialPopulation();
+            Matrix newWeights = oldPopulation.get(genomeIndex).weightMatrix;
+            double newFitness = oldPopulation.get(genomeIndex).fitnessValue;
+            //oldANN.setWeights(newWeights);
+            //oldANN.setFitness(newFitness);
+
+        }else{
+            //Matrix oldWeights = oldANN.getWeights();
+            //double oldFitness = oldANN.getFitness();
+            //genomeFromOldData = createGenome(oldWeights, oldFitness);
+            //newPopulation.set(genomeIndex, genomeFromOldData);
+
         }
-        return oldANN;
+        return returnANN;
+    }
+
+    ANN getANNFromGenome(Genome genome){
+        return new ANN;
     }
 
 
