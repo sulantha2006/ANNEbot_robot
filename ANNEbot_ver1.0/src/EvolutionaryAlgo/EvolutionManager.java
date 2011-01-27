6063d8101b64b46409221ad2a195ba6000ab1409
Sulantha <sulantha@sulantha-virtual-machine.(none)>
2011-01-27 13:55:02 +0530

    modified EvoMan

--- ANNEbot_ver1.0/src/EvolutionaryAlgo/EvolutionManager.java
+++ ANNEbot_ver1.0/src/EvolutionaryAlgo/EvolutionManager.java
@@ -46,25 +46,6 @@ public class EvolutionManager {
     }
 
 
-    Matrix getWeights(ANN myANN){
-        ArrayList<Double> temp = new ArrayList<Double>();
-        //Matrix[] weights= new Matrix[myANN.getInputNeuronCount()+myANN.getHiddenLNeuronCount()+myANN.getOutputNeuronCount()];
-        //double weights[] = new double[myANN.getInputNeuronCount()+myANN.getHiddenLNeuronCount()+myANN.getOutputNeuronCount()];
-        for(int i = 0 ; i < myANN.getLayers().length ; i++){
-            for(int j = 0 ; j < myANN.getLayers()[i].getNeuronCount() ; j++){
-                //weights[i+j] = myANN.getLayers()[i].getNeurons()[j].getInputWeights();
-                for(int k = 0; j < myANN.getLayers()[i].getNeurons()[j].getInputWeights().getNumOfCols(); k++ ){
-                    temp.add(myANN.getLayers()[i].getNeurons()[j].getInputWeights().get(0, k));
-                }
-            }
-        }
-        Matrix weights = new Matrix(1,temp.size());
-        for (int l = 0; l < temp.size(); l++){
-            weights.set(0, l, temp.get(l));
-        }
-        return weights;
-    }
-
     private void createInitialPopulation() {
         throw new UnsupportedOperationException("Not yet implemented");
     }
