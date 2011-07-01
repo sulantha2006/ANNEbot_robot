0318c7832e28659c885f341bfd53ff3fbf5bfcd1
Dilmi <dilmi@ubuntu.(none)>
2011-07-01 07:59:58 -0700

    NeuronModifier class

--- ANNEbot_ver1.0/src/ANNEvolver/NeuronModifier.java
+++ ANNEbot_ver1.0/src/ANNEvolver/NeuronModifier.java
@@ -5,10 +5,31 @@
 
 package ANNEvolver;
 
+import ANN.ANN;
+import Utility.ANNConfiguration;
+
 /**
  *
  * @author sulantha
  */
 public class NeuronModifier {
+    ANN newANN ;
+    public NeuronModifier(){
+        int inputNeuronCount = ANNConfiguration.inputNeuronCountConfig;
+        int hiddenNeuronCount = ANNConfiguration.hiddenLNeuronCountConfig;
+        int outputNeuronCount = ANNConfiguration.outputNeuronCountConfig;
+        this.newANN = new ANN(inputNeuronCount,hiddenNeuronCount,outputNeuronCount);
+    }
+    public void createNewANN(ANN oldAnn){
+        int inputNeuronCount = oldAnn.getInputNeuronCount();
+        int hiddenNeuronCount = oldAnn.getHiddenLNeuronCount() + 1;
+        int outputNeuronCount = oldAnn.getOutputNeuronCount();
+        this.newANN = new ANN(inputNeuronCount,hiddenNeuronCount,outputNeuronCount);
+        this.setNewConnections();
+    }
+
+    public void setNewConnections(){
+        
+    }
 
 }
